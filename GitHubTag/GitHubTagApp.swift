f05ea92c8e00a4a17ec2fc7bdd19eb3a7d31ef32
hamster1963 <1410514192@qq.com>
2023-11-23 11:11:12 +0800

    Initial Commit

---
+++ GitHubTag/GitHubTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTagApp.swift
+//  GitHubTag
+//
+//  Created by Hamster on 2023/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
