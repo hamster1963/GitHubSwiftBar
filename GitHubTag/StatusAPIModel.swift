955faa99c43b2078202bc37033f90ee4e191a45c
hamster1963 <1410514192@qq.com>
2023-12-06 14:42:05 +0800

    update: 更新连接状态检测方法

--- GitHubTag/StatusAPIModel.swift
+++ GitHubTag/StatusAPIModel.swift
@@ -5,55 +5,46 @@
 //  Created by Hamster on 2023/12/4.
 //
 
-import Foundation
 import Combine
-
-
+import Foundation
 
 struct StatusResponse: Codable {
     var code: Int
     var msg: String
 }
 
-
 class StatusViewModel: ObservableObject {
     @Published var status: Bool = false
     var timer: AnyCancellable?
 
     func fetchStatus() {
-        guard let url = URL(string: "https://www.buycoffee.top/api/go-func/getPlaying") else { return }
+        guard let url = URL(string: "https://www.buycoffee.top/api/activity") else { return }
 
-        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
             guard let data = data, error == nil else { return }
-            
+
             // 打印接收到的原始数据
-            // if let jsonString = String(data: data, encoding: .utf8) {
-            //     print("接收到的 JSON 数据: \(jsonString)")
-            // }
-            
-            let decoder = JSONDecoder()
-            do {
-                let response = try decoder.decode(StatusResponse.self, from: data)
-                DispatchQueue.main.async {
-                    if response.code == 200 {
+            if let jsonString = String(data: data, encoding: .utf8) {
+                print("接收到的 JSON 数据: \(jsonString)")
+                if jsonString.starts(with: "data:") {
+                    DispatchQueue.main.async {
                         self?.status = true
-                    } else {
+                    }
+                } else {
+                    DispatchQueue.main.async {
                         self?.status = false
                     }
                 }
-            } catch {
-                print("解码失败: \(error)")
-                self?.status = false
             }
+
         }.resume()
     }
+
     func startFetchingStatus(interval: TimeInterval = 5) {
         fetchStatus()
-            timer = Timer.publish(every: interval, on: .main, in: .common).autoconnect()
-                .sink { [weak self] _ in
-                    self?.fetchStatus()
-                }
-        }
+        timer = Timer.publish(every: interval, on: .main, in: .common).autoconnect()
+            .sink { [weak self] _ in
+                self?.fetchStatus()
+            }
+    }
 }
-
-
